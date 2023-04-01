a87ac881d89500e8855ce2527169c8af58d3b505
Jared Adamson <jared.d.adamson@gmail.com>
2023-04-01 10:47:25 -0700

    Update Module_7.sql

--- Week7/Module_7.sql
+++ Week7/Module_7.sql
@@ -1,6 +1,6 @@
 
 Insert into employees 
-values ('150005', '1985-03-17', 'Jared','Adamson','M','2023-02-21');
+values ('150005', '1995-07-27', 'Jared','Adamson','M','2023-02-21');
 
 Insert into departments(dept_no, dept_name) 
 values ('d010', 'New Department');
